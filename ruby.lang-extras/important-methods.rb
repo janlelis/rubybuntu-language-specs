f2bc85984ba2d54bd81ba3a2940115233fad0112
Jan Lelis <mail@janlelis.de>
2011-09-28 10:51:44 +0200

    ruby: improve important-method detection (no more wrong load! or nil? highlighting) + little fixes (^ and __END__)

--- ruby.lang-extras/important-methods.rb
+++ ruby.lang-extras/important-methods.rb
@@ -1,3 +1,12 @@
 puts (Object.methods | Module.methods | Kernel.methods).select{|e| e =~ /^[a-zA-Z_][a-zA-Z0-9?!_]*$/}.map{ |e|
-  "      <keyword>#{e.to_s.gsub /\?/,'\\?'}#{ '(?=\()' if e.to_s[0] =~ /[A-Z]/ }</keyword>"
-}.uniq.sort
+  suffix = 
+    if e.to_s[0] =~ /[A-Z]/
+      '(?=\()'
+    elsif e.to_s[-1] =~ /[?!]/
+      ''
+    else
+      '\%{no_method_suffix}'
+    end
+  
+  "      <keyword>#{e.to_s.gsub /\?/,'\\?'}#{ suffix }</keyword>"
+}.uniq.sort.reverse
