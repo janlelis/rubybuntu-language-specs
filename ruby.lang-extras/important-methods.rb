a146b2a7a7a43017a83199ca88bd15c14b886a60
Jan Lelis <mail@janlelis.de>
2011-09-29 02:59:34 +0200

    ruby: update important methods (private instance methods like public, protected...)

--- ruby.lang-extras/important-methods.rb
+++ ruby.lang-extras/important-methods.rb
@@ -1,4 +1,17 @@
-puts (Object.methods | Module.methods | Kernel.methods).select{|e| e =~ /^[a-zA-Z_][a-zA-Z0-9?!_]*$/}.map{ |e|
+puts (
+  Object.methods |
+  Module.methods |
+  Kernel.methods |
+  Object.private_instance_methods |
+  Module.private_instance_methods |
+  Kernel.private_instance_methods |
+  Object.protected_instance_methods |
+  Module.protected_instance_methods |
+  Kernel.protected_instance_methods |
+  Object.public_instance_methods |
+  Module.public_instance_methods |
+  Kernel.public_instance_methods
+).select{|e| e =~ /^[a-zA-Z_][a-zA-Z0-9?!_]*$/}.map{ |e|
   suffix = 
     if e.to_s[0] =~ /[A-Z]/
       '(?=\()'
